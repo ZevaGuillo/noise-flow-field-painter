cdcf4e99e8b456b3ba4c4548bc9eeaa6684c2012
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-07-05 22:10:07 -0500

    Second commit

---
+++ src/sketch.js
@@ -0,0 +1,109 @@
+
+
+
+var drawLength = 500;
+var noiseScale = 0.005;
+var strokeLength = 15;
+var imgNames = ['./src/images/img1.jpg']; // Add your image's name here.
+var imgs = [];
+var imgIndex = -1;
+
+var frame;
+
+
+function preload() {
+  // Pre-load all images.
+  for (let i = 0; i < imgNames.length; i++) {
+    imgs.push(loadImage(imgNames[i]));
+  }
+}
+
+
+function setup() {
+  createCanvas(500,500);
+  background(65);
+  changeImage();
+  imgs[0].resize((imgs[0].width*500)/imgs[0].height, (imgs[0].height*500)/imgs[0].width);
+}
+
+function draw() {
+  if (frame > drawLength) {
+    return;
+  }
+  
+  let img = imgs[imgIndex];
+  
+  translate(
+		width / 2 - img.width / 2,
+		height / 2 - img.height / 2
+	);
+  
+  // The smaller the stroke is the more the spawn count increases to capture more detail.
+  let count = map(frame, 0, drawLength, 2, 800);
+  
+  for (let i = 0; i < count; i++) {
+    // Pick a random point on the image.
+    let x = int(random(img.width))
+    let y = int(random(img.height))
+    
+    // Convert coordinates to its index.
+    let index = (y * img.width + x) * 4;
+	
+    // Get the pixel's color values.
+    let r = img.pixels[index];
+    let g = img.pixels[index + 1];
+    let b = img.pixels[index + 2];
+    let a = img.pixels[index + 3];
+    
+    stroke(r, g, b, a);
+    
+    // Start with thick strokes and decrease over time.
+    let sw = map(frame, 0, drawLength, 30, 5);
+    strokeWeight(sw);
+    
+		push();
+			translate(x, y);
+
+			// Rotate according to the noise field so there's a 'flow' to it.
+			let n = noise(x * noiseScale, y * noiseScale);
+			rotate(radians(map(n, 0, 1, -360, 360)));
+
+			let lengthVariation = random(0.75, 1.25);
+			line(0, 0, strokeLength * lengthVariation, 0);
+
+			// Draw a highlight for more detail.
+			stroke(min(r * 3, 255), min(g * 3, 255), min(b * 3, 255), random(100));
+			strokeWeight(sw * 0.5);
+
+			line(0, -sw * 0.15, strokeLength * lengthVariation, -sw * 0.15);
+    pop();
+  }
+  
+  frame++;
+}
+
+
+function changeImage() {
+  background(255);
+  
+  frame = 0;
+  
+  noiseSeed(int(random(1000)));
+  
+  imgIndex++;
+  if (imgIndex >= imgNames.length) {
+    imgIndex = 0;
+  }
+	
+	imgs[imgIndex].loadPixels();
+}
+
+
+function mousePressed() {
+  changeImage();
+}
+
+
+function keyPressed() {
+  saveCanvas("noiseFieldPainter", "jpg");
+}
